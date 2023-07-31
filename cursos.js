6af115f925be198233ade71192b76a5192ed6c7b
vperez00 <125326385+vperez00@users.noreply.github.com>
2023-07-31 18:17:09 -0400

    Add files via upload

---
+++ cursos.js
@@ -0,0 +1,43 @@
+let infoCursos ={
+    'programacion': [
+        {
+            id: 1,
+            titulo: 'Aprende python',
+            lenguaje: 'python',
+            vistas: 15000,
+            nivel: 'basico',
+        },
+        {
+            id: 2,
+            titulo: 'Aprende python',
+            lenguaje: 'python',
+            vistas: 13553,
+            nivel: 'intermedio',
+        },
+        {
+            id: 3,
+            titulo: 'Aprende JavaScript',
+            lenguaje: 'javascript',
+            vistas: 102223,
+            nivel: 'basico',
+        },
+    
+    ],
+    'matematicas': [
+        {
+            id: 1,
+            titulo: 'Aprende calculo',
+            tema: 'calculo',
+            vistas: 12427,
+            nivel: 'basico',
+        },
+        {
+            id: 2,
+            titulo: 'Aprende algebra',
+            tema: 'algebra',
+            vistas: 15722,
+            nivel: 'intermedio',
+        },
+    ]
+    }
+    module.exports.infoCursos = infoCursos;
